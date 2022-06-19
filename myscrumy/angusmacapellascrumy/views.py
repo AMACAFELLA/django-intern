3a7a5cddb0b1b227dc9af0dad0606284ba8f6220
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-06-19 13:49:36 +0200

    Files created
    Task 3

---
+++ myscrumy/angusmacapellascrumy/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+def get_grading_parameters(request):
+    return HttpResponse("Welcome to Django")
+from django.shortcuts import render
+
+# Create your views here.
