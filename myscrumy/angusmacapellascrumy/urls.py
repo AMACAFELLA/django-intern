d42dffa83a550a0e4fe0a22cb80e205639b77851
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-06-21 08:43:10 +0200

    Task 4 updated
    Updated task 4

--- myscrumy/angusmacapellascrumy/urls.py
+++ myscrumy/angusmacapellascrumy/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from angusmacapellascrumy import views 
 
 urlpatterns = [
-    path('', views.get_grading_parameters),
-
+    path('', views.get_grading_parameters, name='get_grading_parameters'),
 ]
